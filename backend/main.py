f833805ddf2788a50b2e753e70b3be890f0a978e
Asmita Mallick <114489333+AsmitaMallick@users.noreply.github.com>
2025-05-27 19:52:54 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -19,7 +19,7 @@ app = FastAPI(title="Preetizen eCommerce API", version="1.0.0")
 # CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://localhost:3000","https://sharp-gently-serval.ngrok-free.app"],
+    allow_origins=["http://localhost:5173", "http://localhost:3000"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
